4b6bc9802860d575374b7bae0c95807a9fdeb6c0
Martin Ecarnot <martin.ecarnot@inra.fr>
2017-02-09 11:43:46 +0100

    add first function

---
+++ R/load.R
@@ -0,0 +1,11 @@
+#' Dummy function
+#'
+#' Add 1 to the input number.
+#' @param x numeric
+#'
+#' @return numeric
+#' @author Martin Ecarnot
+#' @export
+firstFunction <- function(x){
+  return(x + 1)
+}
