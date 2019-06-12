0f9ac11dc104758bb20b58f56ec0079ff8c59077
SOLER <solernicolas@MacBook-Pro-de-SOLER.local>
2019-06-12 11:01:15 +0200

    2

---
+++ Script_R_2019/SIGNE_maha0.R
@@ -0,0 +1,21 @@
+# From matrix cal matrix, cal classes and val matrix, compute Mahalanobis dist from each group 
+
+SIGNE_maha0 = function (scal,class_cal,sval) {
+
+ng=nlevels(class_cal)
+nm=levels(class_cal)
+mdist=matrix(nrow = nrow(sval),ncol = ng)
+
+for (i in 1:ng) {
+  igroup=which(class_cal==nm[i])
+  cm=cov(scal[igroup,]) # Create covariance matrix     # xc=x-matrix(data=1, nrow=54)%*%colMeans(x), cm=t(xc)%*%xc
+  center=colMeans(scal[igroup,])  # rlda$means[i,]%*%rlda$scaling
+  # mdist[,i]=mahalanobis(sval,center,cm)
+  mdist[,i]=mahalanobis(sval,center,ginv(cm), inverted = TRUE)
+}
+
+cl <- factor(nm[max.col(-mdist)],levels = nm)
+pred=list(class=cl,dist=mdist)
+
+return(pred)
+}
